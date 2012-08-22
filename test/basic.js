ed2227c311a2cfb510bcc0fb0b722492e5286151
Carlos Rodriguez <carlos@s8f.org>
2012-08-22 11:19:37 -0700

    assert spec.port

--- test/basic.js
+++ test/basic.js
@@ -1,21 +1,21 @@
 var amino = require('amino')
   , assert = require('assert')
-  , createServer = require('http').createServer
+  , http = require('http')
 
 describe('basic test', function () {
   it('attaches', function () {
     amino
       .use(require('../'))
-      .init({redis: false, service: false});
+      .init({service: false});
 
-    assert.equal(typeof amino.createService, 'function');
+    assert.equal(typeof amino.request, 'function');
   });
-  var server;
-  it('creates a service', function (done) {
-    server = createServer(function (req, res) {
+  var server, service;
+  it('sets up a service', function (done) {
+    server = http.createServer(function (req, res) {
       res.end('cool stuff');
     });
-    var service = amino.createService('cool-stuff@0.1.0', server);
+    service = amino.createService('cool-stuff@0.1.0', server);
     server.on('listening', function () {
       assert.equal(typeof service.spec.port, 'number');
       done();
@@ -23,12 +23,9 @@ describe('basic test', function () {
   });
   it('can request the service', function (done) {
     amino.request('cool-stuff@0.1.x', '/', function (err, res, body) {
+      assert.ifError(err);
       assert.equal(body, 'cool stuff');
       done();
     });
-  });
-  it('can close the server', function (done) {
-    server.on('close', done);
-    server.close();
   });
 });
