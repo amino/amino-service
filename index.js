673f2015e16f0e9c2679fd4f42880302b21b3b72
Carlos Rodriguez <carlos@s8f.org>
2012-08-24 11:48:14 -0700

    use amino.Spec

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var Service = require('./lib/service');
-
 exports.attach = function (options) {
+  var amino = this;
+  var Service = require('./lib/service');
   this.createService = function (name, server) {
     return new Service(name, server, options);
   };
