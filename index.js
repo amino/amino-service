2eb163ce75b6b94ef266750c5c338a151401ecaa
Carlos Rodriguez <carlos@s8f.org>
2012-09-04 21:34:52 -0700

    less fail in some cases

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ exports.attach = function (options) {
   var amino = this;
   var Service = require('./lib/service');
   this.createService = function (name, server) {
-    return new Service(name, server, options);
+    return new Service(amino, name, server, options);
   };
 };
